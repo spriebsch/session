5c0e656981577d2f02211fb221916ee4a187ac5d
Stefan Priebsch <stefan@priebsch.de>
2011-09-08 18:50:23 +0200

    Add example code.

---
+++ example/example.php
@@ -0,0 +1,43 @@
+<?php
+
+require __DIR__ . '/../src/autoload.php';
+
+use spriebsch\session\AbstractSession;
+use spriebsch\session\PhpSessionBackend;
+
+class Session extends AbstractSession
+{
+    public function setUser(User $user)
+    {
+        $this->set('user', $user);
+    }
+    
+    public function hasUser()
+    {
+        return $this->has('user');
+    }
+    
+    public function getUser()
+    {
+        return $this->get('user');
+    }
+}
+
+class User
+{
+}
+
+$backend = new PhpSessionBackend();
+
+$session = new Session($backend);
+$session->configure('session-name', '.example.com');
+$session->start();
+
+if ($session->hasUser()) {
+    $user = $session->getUser();
+} else {
+    $session->setUser(new User());
+}
+
+var_dump($session->hasUser());
+var_dump($session->getUser());
