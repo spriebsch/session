1030370ea0fa108e04b3e184d2c499fb7cb85a7b
Stefan Priebsch <stefan@priebsch.de>
2011-09-08 19:29:34 +0200

    Add another example.

---
+++ example/example2.php
@@ -0,0 +1,30 @@
+<?php
+
+require '/home/steve/projects/session/src/autoload.php';
+
+class Session extends spriebsch\session\AbstractSession
+{
+    public function getCounter()
+    {
+        return $this->get('counter');
+    }
+
+    public function incrementCounter()
+    {
+        if (!$this->has('counter')) {
+            $this->set('counter', 0);
+        }
+    
+        $this->set('counter', $this->get('counter') + 1);
+    }
+}
+
+$session = new Session(new spriebsch\session\PhpSessionBackend());
+$session->configure('session-name', 'localhost');
+
+$session->start('foo');
+$session->incrementCounter();
+
+var_dump($session->getCounter());
+
+$session->commit();
